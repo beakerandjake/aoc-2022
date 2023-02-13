c39016e7f96bf9d4280da5bc1cc580a4420bef8b
beakerandjake <beakerandjake@users.noreply.github.com>
2023-02-13 14:34:20 -0700

    encapsulate priority table

--- src/day_03.js
+++ src/day_03.js
@@ -3,13 +3,6 @@
  * Puzzle Description: https://adventofcode.com/2022/day/3
  */
 
-const lowercase = [...Array(26).keys()].map((x) => String.fromCharCode(x + 97));
-const uppercase = [...Array(26).keys()].map((x) => String.fromCharCode(x + 65));
-const priority = [...lowercase, ...uppercase].reduce((acc, x, index) => {
-  acc[x] = index + 1;
-  return acc;
-}, {});
-
 /**
  * Returns the solution for level one of this puzzle.
  * @param {Object} args - Provides both raw and split input.
@@ -17,16 +10,26 @@ const priority = [...lowercase, ...uppercase].reduce((acc, x, index) => {
  * @param {String[]} args.lines - Array containing each line of the input string.
  * @returns {Number|String}
  */
-export const levelOne = ({ lines }) => lines.reduce((acc, line) => {
-    const half = line.length / 2;
-    const lhsSet = new Set(line.slice(0, half));
-    for (let index = half; index < line.length; index += 1) {
-      if (lhsSet.has(line[index])) {
-        return acc + priority[line[index]];
-      }
-    }
+export const levelOne = (() => {
+  const lowercase = [...Array(26).keys()].map((x) => String.fromCharCode(x + 97));
+  const uppercase = [...Array(26).keys()].map((x) => String.fromCharCode(x + 65));
+  const priority = [...lowercase, ...uppercase].reduce((acc, x, index) => {
+    acc[x] = index + 1;
     return acc;
-  }, 0);
+  }, {});
+
+  return ({ lines }) =>
+    lines.reduce((acc, line) => {
+      const half = line.length / 2;
+      const lhsSet = new Set(line.slice(0, half));
+      for (let index = half; index < line.length; index += 1) {
+        if (lhsSet.has(line[index])) {
+          return acc + priority[line[index]];
+        }
+      }
+      return acc;
+    }, 0);
+})();
 
 /**
  * Returns the solution for level two of this puzzle.
