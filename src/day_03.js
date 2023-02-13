b24ac0da97de26779d9a1d20b41c1569fd41567f
beakerandjake <beakerandjake@users.noreply.github.com>
2023-02-13 14:07:21 -0700

    first pass day 3 level 1

--- src/day_03.js
+++ src/day_03.js
@@ -3,6 +3,13 @@
  * Puzzle Description: https://adventofcode.com/2022/day/3
  */
 
+const lowercase = [...Array(26).keys()].map((x) => String.fromCharCode(x + 97));
+const uppercase = [...Array(26).keys()].map((x) => String.fromCharCode(x + 65));
+const priority = [...lowercase, ...uppercase].reduce((acc, x, index) => {
+  acc[x] = index + 1;
+  return acc;
+}, {});
+
 /**
  * Returns the solution for level one of this puzzle.
  * @param {Object} args - Provides both raw and split input.
@@ -10,8 +17,16 @@
  * @param {String[]} args.lines - Array containing each line of the input string.
  * @returns {Number|String}
  */
-export const levelOne = ({ input, lines }) => {
-  // your code here
+export const levelOne = ({ lines }) => {
+  const result = lines.reduce((acc, line) => {
+    const half = line.length / 2;
+    const compartmentOne = new Set(line.slice(0, half));
+    const compartmentTwo = new Set(line.slice(half));
+    const intersection = [...compartmentOne].filter((x) => compartmentTwo.has(x));
+    return acc + priority[intersection[0]];
+  }, 0);
+
+  return result;
 };
 
 /**
