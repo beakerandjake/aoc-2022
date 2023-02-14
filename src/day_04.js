d9b36e3e17d64aca638a1f955f4658f432a4b783
beakerandjake <beakerandjake@users.noreply.github.com>
2023-02-14 09:06:08 -0700

    first pass day 4 part 1

--- src/day_04.js
+++ src/day_04.js
@@ -3,6 +3,13 @@
  * Puzzle Description: https://adventofcode.com/2022/day/4
  */
 
+const parseRange = (value) => {
+  const split = value.split('-');
+  const lhs = Number.parseInt(split[0], 10);
+  const rhs = Number.parseInt(split[1], 10);
+  return [lhs, rhs];
+};
+
 /**
  * Returns the solution for level one of this puzzle.
  * @param {Object} args - Provides both raw and split input.
@@ -10,8 +17,18 @@
  * @param {String[]} args.lines - Array containing each line of the input string.
  * @returns {Number|String}
  */
-export const levelOne = ({ input, lines }) => {
-  // your code here
+export const levelOne = ({ lines }) => {
+  const toReturn = lines.reduce((acc, line) => {
+    const pairs = line.split(',');
+    const lhs = parseRange(pairs[0]);
+    const rhs = parseRange(pairs[1]);
+    return (lhs[0] >= rhs[0] && lhs[1] <= rhs[1]) ||
+      (rhs[0] >= lhs[0] && rhs[1] <= lhs[1])
+      ? acc + 1
+      : acc;
+  }, 0);
+
+  return toReturn;
 };
 
 /**
