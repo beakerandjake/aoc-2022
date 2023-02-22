a5fe6caba2da711cf7f1a554efce2bc699fff040
beakerandjake <beakerandjake@users.noreply.github.com>
2023-02-22 10:51:00 -0700

    encapsulate to keep pure and remove copying

--- src/day_07.js
+++ src/day_07.js
@@ -8,6 +8,46 @@ const parseLsResult = (line = '') => {
   return lhs === 'dir' ? rhs : +lhs;
 };
 
+const dirName = (parentDir, currentDir) => `${parentDir}_${currentDir}`;
+
+const getDirectories = (lines) => {
+  const directories = {
+    '/': [],
+  };
+
+  const parentMap = {
+    '/': null,
+  };
+
+  let currentDirectory = '/';
+
+  for (let index = 2; index < lines.length; index++) {
+    const line = lines[index];
+
+    if (line[0] === '$') {
+      // ignore ls commands
+      if (line.length === 4) {
+        continue;
+      }
+      // cd to the specified directory, either out (cd ..) or in (cd dirname)
+      currentDirectory =
+        line[5] === '.'
+          ? parentMap[currentDirectory]
+          : dirName(currentDirectory, line.slice(5));
+    } else {
+      let parsed = parseLsResult(line);
+      if (typeof parsed === 'string') {
+        parsed = dirName(currentDirectory, parsed);
+        directories[parsed] = [];
+        parentMap[parsed] = currentDirectory;
+      }
+      directories[currentDirectory].push(parsed);
+    }
+  }
+
+  return directories;
+};
+
 const addDirectory = (directories, newDirName) => ({ ...directories, [newDirName]: [] });
 
 const updateParentMap = (parentMap, newDirName, parentDir) => ({
@@ -29,7 +69,8 @@ const size = (directories, contents) =>
     return acc + x;
   }, 0);
 
-const sumDirectories = (directories) => Object.values(directories).reduce((acc, directory) => {
+const sumDirectories = (directories) =>
+  Object.values(directories).reduce((acc, directory) => {
     const dirSize = size(directories, directory);
 
     if (dirSize <= 100000) {
@@ -46,44 +87,7 @@ const sumDirectories = (directories) => Object.values(directories).reduce((acc, 
  * @param {String[]} args.lines - Array containing each line of the input string.
  * @returns {Number|String}
  */
-export const levelOne = ({ input, lines }) => {
-  let directories = {
-    '/': [],
-  };
-
-  let parentMap = {
-    '/': null,
-  };
-
-  let currentDirectory = '/';
-
-  for (let index = 2; index < lines.length; index++) {
-    const line = lines[index];
-
-    if (line[0] === '$') {
-      // ignore ls commands
-      if (line.length === 4) {
-        continue;
-      }
-      // cd to the specified directory, either out (cd ..) or in (cd dirname)
-      currentDirectory =
-        line[5] === '.'
-          ? parentMap[currentDirectory]
-          : `${currentDirectory}_${line.slice(5)}`;
-    } else {
-      const parsed = parseLsResult(line);
-      if (typeof parsed === 'string') {
-        const dirName = `${currentDirectory}_${parsed}`;
-        directories = addDirectory(directories, dirName);
-        parentMap = updateParentMap(parentMap, dirName, currentDirectory);
-        directories = addToDirectory(directories, currentDirectory, dirName);
-      } else {
-        directories = addToDirectory(directories, currentDirectory, parsed);
-      }
-    }
-  }
-  return sumDirectories(directories);
-};
+export const levelOne = ({ lines }) => sumDirectories(getDirectories(lines));
 
 /**
  * Returns the solution for level two of this puzzle.
