3fb7aff55e7f4c3e45ebce24fcbf8d416d7b3286
beakerandjake <beakerandjake@users.noreply.github.com>
2023-02-23 08:49:22 -0700

    wip day 8 level 1

--- src/day_08.js
+++ src/day_08.js
@@ -3,6 +3,17 @@
  * Puzzle Description: https://adventofcode.com/2022/day/8
  */
 
+const parseInput = (input) => input.split('\n').map((line) => [...line].map((x) => +x));
+
+const countEdges = (grid = []) => grid.length * 4 - 4;
+
+const lookLeft = (row, visitFn) => {
+  const length = row.length - 1;
+  for (let index = 1; index < length; index++) {
+    visitFn(index);
+  }
+};
+
 /**
  * Returns the solution for level one of this puzzle.
  * @param {Object} args - Provides both raw and split input.
@@ -11,7 +22,30 @@
  * @returns {Number|String}
  */
 export const levelOne = ({ input, lines }) => {
-  // your code here
+  const grid = parseInput(input);
+
+  const row = grid[1];
+  const visibleTrees = [];
+  let highest = row[0];
+
+  lookLeft(row, (treeIndex) => {
+    const tree = row[treeIndex];
+    if (tree > highest) {
+      visibleTrees.push(tree);
+      highest = tree;
+    }
+  });
+
+  console.log(`visible trees: ${visibleTrees}`);
+
+  // const nums = [1, 2, 3, 4, 5, 6, 7].map((x) => x.toString());
+  // for (let x = 0; x < nums.length; x++) {
+  //   for (let y = 0; y < nums.length; y++) {
+  //     console.log(`${nums[x]} < ${nums[y]} = ${nums[x] < nums[y]}`);
+  //   }
+  // }
+
+  return 1234;
 };
 
 /**
