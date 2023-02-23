c834f93a2c1f2febc7b0dce857c6197086e6e05d
beakerandjake <beakerandjake@users.noreply.github.com>
2023-02-23 13:51:56 -0700

    remove comments

--- src/day_08.js
+++ src/day_08.js
@@ -8,71 +8,49 @@ const parseInput = (input) => [...input].filter((x) => x !== '\n');
 const getTreeHeight = (grid, rowCount, rowIndex, colIndex) =>
   grid[rowIndex * rowCount + colIndex];
 
-const printTree = (y, x, height) => `[${y},${x}] = ${height}`;
-
 const countVisibleEdges = (height) => height * 4 - 4;
 
 const lookUp = (grid, gridShape, treeY, treeX, treeHeight) => {
-  // console.group(`looking up from: ${printTree(treeY, treeX, treeHeight)}`);
   const [gridHeight] = gridShape;
   for (let y = treeY - 1; y >= 0; y--) {
     const siblingHeight = getTreeHeight(grid, gridHeight, y, treeX);
     if (siblingHeight >= treeHeight) {
-      // console.log(`sibling: ${printTree(y, treeX, siblingHeight)} is taller!`);
-      // console.groupEnd();
       return false;
     }
-    // console.log(`sibling: ${printTree(y, treeX, siblingHeight)}`);
   }
-  // console.groupEnd();
   return true;
 };
 
 const lookDown = (grid, gridShape, treeY, treeX, treeHeight) => {
-  // console.group(`looking down from: ${printTree(treeY, treeX, treeHeight)}`);
   const [gridHeight] = gridShape;
   for (let y = treeY + 1; y < gridHeight; y++) {
     const siblingHeight = getTreeHeight(grid, gridHeight, y, treeX);
     if (siblingHeight >= treeHeight) {
-      // console.log(`sibling: ${printTree(y, treeX, siblingHeight)} is taller!`);
-      // console.groupEnd();
       return false;
     }
-    // console.log(`sibling: ${printTree(y, treeX, siblingHeight)}`);
   }
-  // console.groupEnd();
   return true;
 };
 
 const lookRight = (grid, gridShape, treeY, treeX, treeHeight) => {
-  // console.group(`looking right from: ${printTree(treeY, treeX, treeHeight)}`);
   const [gridHeight, gridWidth] = gridShape;
   for (let x = treeX + 1; x < gridWidth; x++) {
     const siblingHeight = getTreeHeight(grid, gridHeight, treeY, x);
     if (siblingHeight >= treeHeight) {
-      // console.log(`sibling: ${printTree(treeY, x, siblingHeight)} is taller!`);
-      // console.groupEnd();
       return false;
     }
-    // console.log(`sibling: ${printTree(treeY, x, siblingHeight)}`);
   }
-  // console.groupEnd();
   return true;
 };
 
 const lookLeft = (grid, gridShape, treeY, treeX, treeHeight) => {
-  // console.group(`looking right from: ${printTree(treeY, treeX, treeHeight)}`);
-  const [gridHeight, gridWidth] = gridShape;
+  const [gridHeight] = gridShape;
   for (let x = treeX - 1; x >= 0; x--) {
     const siblingHeight = getTreeHeight(grid, gridHeight, treeY, x);
     if (siblingHeight >= treeHeight) {
-      // console.log(`sibling: ${printTree(treeY, x, siblingHeight)} is taller!`);
-      // console.groupEnd();
       return false;
     }
-    // console.log(`sibling: ${printTree(treeY, x, siblingHeight)}`);
   }
-  // console.groupEnd();
   return true;
 };
 
@@ -84,7 +62,6 @@ const lookLeft = (grid, gridShape, treeY, treeX, treeHeight) => {
  * @returns {Number|String}
  */
 export const levelOne = ({ input, lines }) => {
-  console.log();
   const grid = parseInput(input);
   const shape = [lines.length, lines[0].length];
   let visibleCount = countVisibleEdges(shape[0]);
@@ -104,13 +81,6 @@ export const levelOne = ({ input, lines }) => {
       }
     }
   }
-
-  // const [y, x] = [2, 1];
-  // const tree = getTreeHeight(grid, shape[0], y, x);
-  // lookUp(grid, shape, y, x, tree);
-  // lookDown(grid, shape, y, x, tree);
-  // lookRight(grid, shape, y, x, tree);
-  // lookLeft(grid, shape, y, x, tree);
 
   return visibleCount;
 };
