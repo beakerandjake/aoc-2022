6d55b4344a7822f4c169515672d468658178ab6d
beakerandjake <beakerandjake@users.noreply.github.com>
2023-02-22 18:32:53 -0700

    remove comments

--- src/day_07.js
+++ src/day_07.js
@@ -95,10 +95,6 @@ const dirSize = (dirs, dir) =>
 
 /**
  * Returns the solution for level one of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
  */
 export const levelOne = ({ lines }) => {
   const dirs = parseInput(lines);
@@ -110,10 +106,6 @@ export const levelOne = ({ lines }) => {
 
 /**
  * Returns the solution for level two of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
  */
 export const levelTwo = ({ lines }) => {
   const dirs = parseInput(lines);
