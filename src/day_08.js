7800d07041423f0e57c737c2955069d96ab66436
beakerandjake <beakerandjake@users.noreply.github.com>
2023-02-24 13:22:27 -0700

    clean up, reset runtime for new code

--- src/day_08.js
+++ src/day_08.js
@@ -3,33 +3,22 @@
  * Puzzle Description: https://adventofcode.com/2022/day/8
  */
 
+/**
+ * Returns an array of all characters in the input.
+ * The array is a C contiguous 2d array.
+ * @param {String} input
+ * @returns {String[]}
+ */
 const parseInput = (input) => [...input].filter((x) => x !== '\n');
 
-const iterateRow = (treeCount, rowIndex, visitFn) => {
-  const innerWidth = treeCount - 1;
-  for (let x = 1; x < innerWidth; x++) {
-    visitFn(rowIndex, x);
-  }
-};
-
-const iterateRowReverse = (treeCount, rowIndex, visitFn) => {
-  for (let x = treeCount - 2; x > 0; x--) {
-    visitFn(rowIndex, x);
-  }
-};
-
-const iterateColReverse = (treeCount, colIndex, visitFn) => {
-  for (let y = treeCount - 2; y > 0; y--) {
-    visitFn(y, colIndex);
-  }
-};
-
-const iterateCol = (treeCount, colIndex, visitFn) => {
-  const innerHeight = treeCount - 1;
-  for (let y = 1; y < innerHeight; y++) {
-    visitFn(y, colIndex);
-  }
-};
+/**
+ * Returns the value of the tree at the specified coordinate on the grid.
+ * @param {Array} grid
+ * @param {Number} length
+ * @param {Number} rowIndex
+ * @param {Number} colIndex
+ */
+const getTree = (grid, length, rowIndex, colIndex) => grid[rowIndex * length + colIndex];
 
 /**
  * Returns the solution for level one of this puzzle.
@@ -38,38 +27,106 @@ const iterateCol = (treeCount, colIndex, visitFn) => {
  * @param {String[]} args.lines - Array containing each line of the input string.
  * @returns {Number|String}
  */
-export const levelOne = ({ input, lines }) => {
-  const grid = parseInput(input);
-  const treeCount = lines.length; // assuming grid is square of NxN trees.
-  const visibleTrees = new Set();
-
-  const getTree = (rowIndex, colIndex) => grid[rowIndex * treeCount + colIndex];
-
-  const findVisibleTrees = (edge) => {
-    let tallest = edge;
-    return (y, x) => {
-      const height = getTree(y, x);
-      if (height > tallest) {
-        visibleTrees.add(`${y},${x}`);
-        tallest = height;
-      }
-    };
+export const levelOne = (() => {
+  /**
+   * Iterate the row from left to right, visiting each item.
+   * @param {Number} treeCount
+   * @param {Number} rowIndex
+   * @param {Function} visitFn
+   */
+  const iterateRow = (treeCount, rowIndex, visitFn) => {
+    const innerWidth = treeCount - 1;
+    for (let x = 1; x < innerWidth; x++) {
+      visitFn(rowIndex, x);
+    }
   };
 
-  const innerWidth = treeCount - 1;
-  for (let index = 1; index < innerWidth; index++) {
-    // look at row from left to right, starting at left most edge.
-    iterateRow(treeCount, index, findVisibleTrees(getTree(index, 0)));
-    // look at row from right to left, starting at right most edge.
-    iterateRowReverse(treeCount, index, findVisibleTrees(getTree(index, treeCount - 1)));
-    // look at column from top to bottom, starting at top most edge.
-    iterateCol(treeCount, index, findVisibleTrees(getTree(0, index)));
-    // look at column from bottom to top, starting at bottom most edge.
-    iterateColReverse(treeCount, index, findVisibleTrees(getTree(treeCount - 1, index)));
-  }
+  /**
+   * Iterate the row from right to left, visiting each item.
+   * @param {Number} treeCount
+   * @param {Number} rowIndex
+   * @param {Function} visitFn
+   */
+  const iterateRowReverse = (treeCount, rowIndex, visitFn) => {
+    for (let x = treeCount - 2; x > 0; x--) {
+      visitFn(rowIndex, x);
+    }
+  };
 
-  return treeCount * 4 - 4 + visibleTrees.size;
-};
+  /**
+   * Iterate the column from top to bottom, visiting each item.
+   * @param {Number} treeCount
+   * @param {Number} rowIndex
+   * @param {Function} visitFn
+   */
+  const iterateCol = (treeCount, colIndex, visitFn) => {
+    const innerHeight = treeCount - 1;
+    for (let y = 1; y < innerHeight; y++) {
+      visitFn(y, colIndex);
+    }
+  };
+
+  /**
+   * Iterate the column from bottom to top, visiting each item.
+   * @param {Number} treeCount
+   * @param {Number} rowIndex
+   * @param {Function} visitFn
+   */
+  const iterateColReverse = (treeCount, colIndex, visitFn) => {
+    for (let y = treeCount - 2; y > 0; y--) {
+      visitFn(y, colIndex);
+    }
+  };
+
+  /**
+   * Returns all of the edges from a given row/column index in the grid.
+   * @param {Array} grid
+   * @param {Number} length
+   * @param {Number} index
+   */
+  const getEdges = (grid, length, index) => ({
+    top: getTree(grid, length, 0, index),
+    bottom: getTree(grid, length, length - 1, index),
+    left: getTree(grid, length, index, 0),
+    right: getTree(grid, length, index, length - 1),
+  });
+
+  /**
+   * Given the length of a 2d grid, returns the number of visible edges.
+   * @param {Number} length
+   */
+  const countVisibleEdges = (length) => length * 4 - 4;
+
+  return ({ input, lines }) => {
+    const grid = parseInput(input);
+    const { length } = lines; // assuming grid is square of NxN trees.
+    const visibleTrees = new Set();
+
+    const findVisibleTrees = (edge) => {
+      let tallest = edge;
+      return (y, x) => {
+        const height = getTree(grid, length, y, x);
+        if (height > tallest) {
+          visibleTrees.add(`${y},${x}`);
+          tallest = height;
+        }
+      };
+    };
+
+    // Instead of looking at each tree individually from its own perspective,
+    // perform a "walk" around the grid looking inward and count each
+    // tree visible from the "outside"
+    for (let index = length - 2; index > 0; index--) {
+      const edges = getEdges(grid, length, index);
+      iterateRow(length, index, findVisibleTrees(edges.left));
+      iterateRowReverse(length, index, findVisibleTrees(edges.right));
+      iterateCol(length, index, findVisibleTrees(edges.top));
+      iterateColReverse(length, index, findVisibleTrees(edges.bottom));
+    }
+
+    return countVisibleEdges(length) + visibleTrees.size;
+  };
+})();
 
 /**
  * Returns the solution for level two of this puzzle.
