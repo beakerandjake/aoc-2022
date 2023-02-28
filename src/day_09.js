015dd95727e2dbcbb3d064bf9f33fe6992002821
beakerandjake <beakerandjake@users.noreply.github.com>
2023-02-28 14:56:23 -0700

    cleanup

--- src/day_09.js
+++ src/day_09.js
@@ -10,7 +10,7 @@ class Vector2 {
   }
 
   toString() {
-    return `<${this.x},${this.y}>`;
+    return `${this.x} ${this.y}`;
   }
 }
 
@@ -20,32 +20,12 @@ const distanceSquared = (lhs, rhs) => (lhs.y - rhs.y) ** 2 + (lhs.x - rhs.x) ** 
 
 const touching = (head, tail) => distanceSquared(head, tail) <= 2;
 
-const copy = (vector) => new Vector2(vector.x, vector.y);
-
 const directions = {
   U: new Vector2(0, 1),
   D: new Vector2(0, -1),
   L: new Vector2(-1, 0),
   R: new Vector2(1, 0),
 };
-
-const parseLine = (line) => {
-  const [direction, steps] = line.split(' ');
-  return { direction: directions[direction], steps: +steps };
-};
-
-const headMovementPlan = (start, direction, steps) => {
-  const positions = [];
-  let current = start;
-  while (positions.length < steps) {
-    current = add(current, direction);
-    positions.push(current);
-  }
-  return positions;
-};
-
-const newTailPosition = (tail, currentHead, newHead) =>
-  touching(tail, newHead) ? copy(tail) : copy(currentHead);
 
 /**
  * Returns the solution for level one of this puzzle.
@@ -54,24 +34,48 @@ const newTailPosition = (tail, currentHead, newHead) =>
  * @param {String[]} args.lines - Array containing each line of the input string.
  * @returns {Number|String}
  */
-export const levelOne = ({ input, lines }) => {
-  let head = new Vector2(0, 0);
-  let tail = new Vector2(0, 0);
-  const visited = new Set([tail.toString()]);
+export const levelOne = (() => {
+  const parseLine = (line) => {
+    const [direction, steps] = line.split(' ');
+    return { direction: directions[direction], steps: +steps };
+  };
 
-  lines.forEach((line) => {
-    const { direction, steps } = parseLine(line);
-    const headPlan = headMovementPlan(head, direction, steps);
-    headPlan.forEach((newHead) => {
-      tail = newTailPosition(tail, head, newHead);
-      head = newHead;
-      visited.add(tail.toString());
+  const headMovementPlan = (start, direction, steps) => {
+    const positions = [];
+    let current = start;
+    while (positions.length < steps) {
+      current = add(current, direction);
+      positions.push(current);
+    }
+    return positions;
+  };
+
+  const tailMovementPlan = (tail, head, headPlan) => {
+    let previousHead = head;
+    return headPlan.map((newHead) => {
+      const newTail = touching(tail, newHead) ? tail : previousHead;
+      previousHead = newHead;
+      return newTail;
+    });
+  };
+
+  return ({ lines }) => {
+    let head = new Vector2(0, 0);
+    let tail = new Vector2(0, 0);
+    const visited = new Set([tail.toString()]);
+
+    lines.forEach((line) => {
+      const { direction, steps } = parseLine(line);
+      const headPlan = headMovementPlan(head, direction, steps);
+      const tailPlan = tailMovementPlan(tail, head, headPlan);
+      tailPlan.forEach((x) => visited.add(x.toString()));
+      head = headPlan[headPlan.length - 1];
+      tail = tailPlan[tailPlan.length - 1];
     });
 
-  });
-
-  return visited.size;
-};
+    return visited.size;
+  };
+})();
 
 /**
  * Returns the solution for level two of this puzzle.
