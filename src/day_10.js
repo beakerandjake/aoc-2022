85c982880304efda52e4b7d202fae309298b97a6
beakerandjake <beakerandjake@users.noreply.github.com>
2023-03-03 12:19:23 -0700

    testing out faster execution

--- src/day_10.js
+++ src/day_10.js
@@ -26,17 +26,45 @@ export const levelOne = (() => {
     });
   };
 
+  const parseLines = (lines) => {
+    let cycle = 0;
+    let register = 1;
+    return lines.map((line) => {
+      const { value, cycleCount } =
+        line[0] === 'n'
+          ? { value: 0, cycleCount: 1 }
+          : { value: +line.slice(4), cycleCount: 2 };
+      cycle += cycleCount;
+      register += value;
+      return { cycle, register };
+    });
+  };
+
+  const getRegisterValue = (cycles, cycle) => {
+    let found = cycles[0];
+    for (let index = 1; index < cycles.length; index++) {
+      const current = cycles[index];
+      if (current.cycle >= cycle) {
+        break;
+      }
+      found = current;
+    }
+    return found.register;
+  };
+
   const signalCycles = [20, 60, 100, 140, 180, 220];
   const calculateTotalSignalStrength = (cycles) =>
     signalCycles.reduce(
-      (total, cycle) =>
-        total + cycle * cycles.filter((x) => x.cycle < cycle).reverse()[0].register,
+      // (total, cycle) =>
+      //   total + cycle * cycles.filter((x) => x.cycle < cycle).reverse()[0].register,
+      (total, cycle) => total + cycle * getRegisterValue(cycles, cycle),
       0
     );
 
   return ({ lines }) => {
-    const instructions = lines.map(parseLine);
-    const cycles = applyInstructions(instructions);
+    // const instructions = lines.map(parseLine);
+    // const cycles = applyInstructions(instructions);
+    const cycles = parseLines(lines);
     return calculateTotalSignalStrength(cycles);
   };
 })();
