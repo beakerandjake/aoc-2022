e95fd2e7f22cc70bd97cccb162f224ada03bd8ae
beakerandjake <beakerandjake@users.noreply.github.com>
2023-03-03 12:07:46 -0700

    faster runtime

--- src/day_10.js
+++ src/day_10.js
@@ -8,28 +8,6 @@ const parseLine = (line) =>
     ? { name: 'noop', value: 0, cycleCount: 1 }
     : { name: 'addx', value: +line.slice(4), cycleCount: 2 };
 
-const applyInstructions = (instructions) => {
-  let cycle = 0;
-  let register = 1;
-  return instructions.map(({ value, cycleCount }) => {
-    cycle += cycleCount;
-    register += value;
-    return { cycle, register };
-  });
-};
-
-const filterCycles = (cycles, maxCycle) => cycles.filter((x) => x.cycle < maxCycle);
-
-const getRegisterValue = (cycles) => cycles[cycles.length - 1].register;
-
-const calculateTotalSignalStrength = (cycles) => {
-  const signalCycles = [20, 60, 100, 140, 180, 220];
-  const registerValues = signalCycles.map(
-    (cycle) => cycle * getRegisterValue(filterCycles(cycles, cycle))
-  );
-  return registerValues.reduce((total, x) => total + x, 0);
-};
-
 /**
  * Returns the solution for level one of this puzzle.
  * @param {Object} args - Provides both raw and split input.
@@ -37,11 +15,31 @@ const calculateTotalSignalStrength = (cycles) => {
  * @param {String[]} args.lines - Array containing each line of the input string.
  * @returns {Number|String}
  */
-export const levelOne = ({ lines }) => {
-  const instructions = lines.map(parseLine);
-  const cycles = applyInstructions(instructions);
-  return calculateTotalSignalStrength(cycles);
-};
+export const levelOne = (() => {
+  const applyInstructions = (instructions) => {
+    let cycle = 0;
+    let register = 1;
+    return instructions.map(({ value, cycleCount }) => {
+      cycle += cycleCount;
+      register += value;
+      return { cycle, register };
+    });
+  };
+
+  const signalCycles = [20, 60, 100, 140, 180, 220];
+  const calculateTotalSignalStrength = (cycles) =>
+    signalCycles.reduce(
+      (total, cycle) =>
+        total + cycle * cycles.filter((x) => x.cycle < cycle).reverse()[0].register,
+      0
+    );
+
+  return ({ lines }) => {
+    const instructions = lines.map(parseLine);
+    const cycles = applyInstructions(instructions);
+    return calculateTotalSignalStrength(cycles);
+  };
+})();
 
 /**
  * Returns the solution for level two of this puzzle.
