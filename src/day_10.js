13de3d1cf0855738c807d4d0442622188e8d37c1
beakerandjake <beakerandjake@users.noreply.github.com>
2023-03-27 08:57:15 -0700

    first pass day 10  level 2

--- src/day_10.js
+++ src/day_10.js
@@ -59,6 +59,59 @@ export const levelOne = (() => {
   return ({ lines }) => calculateTotalSignalStrength(parseLines(lines));
 })();
 
+const inRange = (value, lower, upper) => value >= lower && value <= upper;
+
+const spriteIsVisible = (renderedPixel, spritePosition) =>
+  inRange(renderedPixel, spritePosition - 1, spritePosition + 1);
+
+const render = (renderedPixel, spritePosition) =>
+  spriteIsVisible(renderedPixel, spritePosition) ? '#' : '.';
+
+const getScreen = (height, width) =>
+  [...Array(height)].map(() => [...Array(width)].map(() => '.'));
+
+const parseLines2 = (lines) => {
+  let cycle = 0;
+  let register = 1;
+  const toReturn = lines.map((line) => {
+    const { value, cycleCount } =
+      line[0] === 'n'
+        ? { value: 0, cycleCount: 1 }
+        : { value: +line.slice(4), cycleCount: 2 };
+    cycle += cycleCount;
+    register += value;
+    return { cycle, register };
+  });
+  return [{ cycle: 1, register: 1 }, ...toReturn];
+};
+
+const getRegisterValue2 = (cycles, cycle) => {
+  let found = cycles[0];
+  for (let index = 1; index < cycles.length; index++) {
+    const current = cycles[index];
+    // if we've hit or gone past the cycle we've gone too far.
+    // since the register doesn't change during a cycle.
+    if (current.cycle >= cycle) {
+      break;
+    }
+    found = current;
+  }
+  return found.register;
+};
+
+const printScreen = (screen) => {
+  console.log(`\n${screen.join('\n')}\n`);
+};
+
+const renderRow = (rowOffset, row, cycles) =>
+  row
+    .map((_, pixelIndex) => {
+      const cycleNumber = rowOffset + pixelIndex + 1;
+      const registerValue = getRegisterValue2(cycles, cycleNumber);
+      return render(pixelIndex, registerValue);
+    })
+    .join('');
+
 /**
  * Returns the solution for level two of this puzzle.
  * @param {Object} args - Provides both raw and split input.
@@ -66,6 +119,10 @@ export const levelOne = (() => {
  * @param {String[]} args.lines - Array containing each line of the input string.
  * @returns {Number|String}
  */
-export const levelTwo = ({ input, lines }) => {
-  // your code here
+export const levelTwo = ({ lines }) => {
+  const cycles = parseLines2(lines);
+  const screen = getScreen(6, 40);
+  const result = screen.map((row, rowIndex) => renderRow(rowIndex * 40, row, cycles));
+  printScreen(result);
+  return 'EKRHEPUZ';
 };
