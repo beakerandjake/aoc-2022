2765139ef09b2e7a68d8130bf858d6aebf14ebb8
beakerandjake <beakerandjake@users.noreply.github.com>
2023-03-30 13:55:36 -0700

    more util functions

--- src/util.js
+++ src/util.js
@@ -1,4 +1,12 @@
 /**
+ * Checks if value is between start and up to, but not including end.
+ * @param {Number} value
+ * @param {Number} start
+ * @param {Number} end
+ */
+export const inRange = (value, start, end) => value >= start && value < end;
+
+/**
  * Removes the first element of the array.
  * Does not mutate the original array but instead returns a new copy.
  * @param {Array} arr
@@ -38,18 +46,13 @@ export const parse2dArray = (input, characterMapFn = null, rowSeparator = '\n') 
   const toReturn = [];
   const { length } = input;
   let rowCount = 0;
-  let colIndex = 0;
 
   for (let index = 0; index < length; index++) {
     const character = input[index];
     if (character !== rowSeparator) {
-      toReturn.push(
-        characterMapFn ? characterMapFn(character, rowCount, colIndex) : character
-      );
-      colIndex++;
+      toReturn.push(characterMapFn ? characterMapFn(character) : character);
     } else {
       rowCount++;
-      colIndex = 0;
     }
   }
 
@@ -68,6 +71,16 @@ export const parse2dArray = (input, characterMapFn = null, rowSeparator = '\n') 
 export const index2d = (width, y, x) => width * y + x;
 
 /**
+ * Returns the coordinate (y,x) from the index of the element in a *flattened* representation of the 2d array.
+ * @param {Number} width - The number of elements in each row of the 2d array.
+ * @param {Number} index
+ */
+export const indexToCoordinate2d = (width, index) => ({
+  y: Math.floor(index / width),
+  x: index % width,
+});
+
+/**
  * Returns the element at <y,x> in the *flattened* 2d array.
  * @param {Array} array -  The *flattened* 2d array.
  * @param {Object} shape - The shape of the 2d array (width and height)
@@ -83,6 +96,43 @@ export const elementAt2d = (array, { width }, y, x) => array[index2d(width, y, x
  * @returns
  */
 export const getShape = (input) => ({ height: input.length, width: input[0].length });
+
+/**
+ * Returns all of the neighbors
+ */
+const neighbors2d = (grid, { width, height }, y, x, neighborDeltas) =>
+  neighborDeltas.reduce((neighbors, delta) => {
+    const dY = delta.y + y;
+    const dX = delta.x + x;
+    if (dY >= 0 && dY < height && dX >= 0 && dX < width) {
+      neighbors.push(grid[index2d(width, dY, dX)]);
+    }
+    return neighbors;
+  }, []);
+
+/**
+ * Returns all of the N,S,E,W neighbors of the element at [y,x]
+ */
+export const cardinalNeighbors2d = (() => {
+  const deltas = [
+    { y: -1, x: 0 },
+    { y: 1, x: 0 },
+    { y: 0, x: -1 },
+    { y: 0, x: 1 },
+  ];
+  return (grid, shape, y, x) => neighbors2d(grid, shape, y, x, deltas);
+})();
+
+export const forEach2d = (array, { height, width }, callbackFn) => {
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const result = callbackFn(array[index2d(width, y, x)], y, x);
+      if (result === false) {
+        break;
+      }
+    }
+  }
+};
 
 /**
  * Converts the string to a number.
@@ -111,11 +161,3 @@ export const multiply = (a, b) => a * b;
  * @param {RegExp} regex
  */
 export const firstCapture = (str, regex) => str.match(regex)[1];
-
-/**
- * Checks if value is between start and up to, but not including end.
- * @param {Number} value
- * @param {Number} start
- * @param {Number} end
- */
-export const inRange = (value, start, end) => value >= start && value < end;
