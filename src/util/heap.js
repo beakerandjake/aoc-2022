6dfa5e0a4496d50c0bd950ccf03d5afaefe48451
beakerandjake <beakerandjake@users.noreply.github.com>
2023-04-05 14:39:14 -0400

    update heap

--- src/util/heap.js
+++ src/util/heap.js
@@ -1,79 +1,82 @@
-import { greaterThan, lessThan } from './logic.js';
-
 /**
- * Returns the index which corresponds to the nodes left child.
+ * Base class for a Min or a Max heap.
  */
-const leftChild = (index) => index * 2;
+export class Heap {
+  _items = [];
+  _maxIndex = 0;
 
-/**
- * Returns the index which corresponds to the nodes right child.
- */
-const rightChild = (index) => index * 2 + 1;
+  constructor(items, compareFn) {
+    this._compareFn = compareFn;
+    items.forEach((item) => this.push(item));
+  }
 
-/**
- * Returns the index of the nodes parent.
- */
-const parent = (index) => Math.floor(index / 2);
+  /**
+   * Returns the index of the nodes left child.
+   */
+  static _leftChild = (index) => index * 2;
 
-/**
- * Returns the max index of the heap.
- */
-const maxIndex = (heap) => heap.length - 1;
+  /**
+   * Returns the index of the nodes right child.
+   */
+  static _rightChild = (index) => index * 2 + 1;
 
-/**
- * Swap elements in the heap.
- * This method modifies the heap.
- */
-const swap = (heap, lhsIndex, rhsIndex) => {
-  const temp = heap[lhsIndex];
-  // eslint-disable-next-line no-param-reassign
-  heap[lhsIndex] = heap[rhsIndex];
-  // eslint-disable-next-line no-param-reassign
-  heap[rhsIndex] = temp;
-};
+  /**
+   * Returns the index of the nodes parent.
+   */
+  static _parent = (index) => Math.floor(index / 2);
 
-const createHeapFunctions = (compareFn) => {
-  const compare = (heap, lhsIndex, rhsIndex) =>
-    compareFn(heap[lhsIndex].priority, heap[rhsIndex].priority);
+  /**
+   * Compare the two items using the heap comparison function.
+   */
+  _compare = (lhsIndex, rhsIndex) =>
+    this._compareFn(this._items[lhsIndex].priority, this._items[rhsIndex].priority);
+
+  /**
+   * Swap elements in the heap.
+   * This method modifies the heap.
+   */
+  _swap = (lhsIndex, rhsIndex) => {
+    const temp = this._items[lhsIndex];
+    this._items[lhsIndex] = this._items[rhsIndex];
+    this._items[rhsIndex] = temp;
+  };
 
   /**
    * Bubble the element up the heap until the heap property is satisfied.
-   * This method modifies the heap.
    */
-  const bubbleUp = (heap, index) => {
+  _bubbleUp = (index) => {
     let currentIndex = index;
-    let parentIndex = parent(currentIndex);
-    while (parentIndex >= 1 && compare(heap, parentIndex, currentIndex)) {
-      swap(heap, parentIndex, currentIndex);
+    let parentIndex = Heap._parent(currentIndex);
+    while (parentIndex >= 1 && this._compare(parentIndex, currentIndex) < 0) {
+      this._swap(parentIndex, currentIndex);
       currentIndex = parentIndex;
-      parentIndex = parent(currentIndex);
+      parentIndex = Heap._parent(currentIndex);
     }
   };
 
   /**
-   * Compares a node to its child and returns the index of the node
-   * which satisfies the heap property.
-   */
-  const compareToChild = (heap, lastIndex, index, childIndex) =>
-    childIndex <= lastIndex && compare(heap, index, childIndex) ? childIndex : index;
-
-  /**
    * Bubble the element down the heap until the heap property is satisfied.
-   * This method modifies the heap.
    */
-  const bubbleDown = (heap, index) => {
-    const lastIndex = maxIndex(heap);
+  _bubbleDown = (index) => {
     let currentIndex = index;
-    while (currentIndex <= lastIndex) {
+    while (currentIndex <= this._maxIndex) {
       let swapIndex = currentIndex;
-      swapIndex = compareToChild(heap, lastIndex, swapIndex, leftChild(currentIndex));
-      swapIndex = compareToChild(heap, lastIndex, swapIndex, rightChild(currentIndex));
+
+      const left = Heap._leftChild(currentIndex);
+      if (left <= this._maxIndex && this._compare(swapIndex, left) < 0) {
+        swapIndex = left;
+      }
+
+      const right = Heap._rightChild(currentIndex);
+      if (right <= this._maxIndex && this._compare(swapIndex, right) < 0) {
+        swapIndex = right;
+      }
 
       if (swapIndex === currentIndex) {
         break;
       }
 
-      swap(heap, currentIndex, swapIndex);
+      this._swap(currentIndex, swapIndex);
       currentIndex = swapIndex;
     }
   };
@@ -81,72 +84,94 @@ const createHeapFunctions = (compareFn) => {
   /**
    * Returns the head element of the heap.
    */
-  const peek = (heap) => heap[1];
+  peek = () => {
+    if (this._maxIndex === 0) {
+      return undefined;
+    }
+    return this._items[1];
+  };
 
   /**
    * Adds a new element to the heap.
-   * This method does not modify the heap but returns a new copy.
    */
-  const push = (heap, item) => {
-    // push item to end of heap
-    const toReturn = heap.length ? [...heap, item] : [undefined, item];
-
-    bubbleUp(toReturn, maxIndex(toReturn));
-    return toReturn;
-  };
+  push(item) {
+    this._maxIndex += 1;
+    this._items[this._maxIndex] = item;
+    this._bubbleUp(this._maxIndex);
+  }
 
   /**
-   * Removes the head element of the heap.
-   * This method does not modify the heap but returns a new copy.
+   * Removes the head element from the heap and returns it.
    */
-  const pop = (heap) => {
-    if (heap.length <= 2) {
-      return [];
+  pop() {
+    if (this._maxIndex === 0) {
+      return undefined;
     }
+    const popped = this._items[1];
 
-    // remove the head node and promote the last node to the head.
-    const toReturn = [...heap];
-    toReturn[1] = toReturn.pop();
-    bubbleDown(toReturn, 1);
+    this._maxIndex -= 1;
+    this._items[1] = popped;
+    this._bubbleDown(1);
 
-    return toReturn;
-  };
+    return popped;
+  }
 
-  /**
-   * Updates the priority of the element at the specified index.
-   * This method does not modify the heap but returns a new copy.
-   */
-  const update = (heap, index, priority) => {
-    const toReturn = [...heap];
+  update = (item, newPriority) => {
+    const index = this._items.indexOf(item);
+
+    if (index === -1) {
+      return false;
+    }
 
     // update the priority of the node.
-    const oldValue = toReturn[index].priority;
-    toReturn[index].priority = priority;
+    const oldPriority = this._items[index].priority;
+    this._items[index].priority = newPriority;
 
     // move the node up or down the heap to satisfy the heap property.
-    if (compareFn(oldValue, priority)) {
-      bubbleUp(toReturn, index);
+    if (this._compareFn(oldPriority, newPriority) < 0) {
+      this._bubbleUp(index);
     } else {
-      bubbleDown(toReturn, index);
+      this._bubbleDown(index);
     }
 
-    return toReturn;
+    return true;
   };
+}
 
-  return {
-    peek,
-    pop,
-    push,
-    update,
-  };
+/**
+ * Comparison function which satisfies the max heap property.
+ * @returns {Number} 1 if a should move towards the top of the heap, -1 if a should move towards the bottom, 0 if a and b are equal.
+ */
+const maxHeapCompare = (a, b) => {
+  if (a < b) {
+    return -1;
+  }
+  if (a > b) {
+    return 1;
+  }
+  return 0;
 };
 
 /**
- * Functions for working with a min heap.
+ * Returns a new max heap, which keeps the max element at the top of the heap.
  */
-export const minHeap = createHeapFunctions(greaterThan);
+export const maxHeap = (items = []) => new Heap(items, maxHeapCompare);
 
 /**
- * Functions for working with a max heap.
+ * Comparison function which satisfies the min heap property.
+ * @returns {Number} 1 if a should move towards the top of the heap, -1 if a should move towards the bottom, 0 if a and b are equal.
  */
-export const maxHeap = createHeapFunctions(lessThan);
+const minHeapCompare = (a, b) => {
+  if (a > b) {
+    return -1;
+  }
+  if (a < b) {
+    return 1;
+  }
+  return 0;
+};
+
+/**
+ * Returns a new min heap, which keeps the min element at the top of the heap.
+ */
+export const minHeap = (items = []) => new Heap(items, minHeapCompare);
