ca52804110bb138b7bb5a6a2cf6af6bf1af40732
beakerandjake <beakerandjake@users.noreply.github.com>
2023-04-17 12:08:45 -0400

    some clean up

--- src/day_14.js
+++ src/day_14.js
@@ -41,16 +41,17 @@ const parseLines = (() => {
       : numbersInBetween(start.x, end.x).map((x) => new Vector2(x, start.y));
 
   /**
+   * Returns all points by recursively tracing each point of the path backwards.
+   */
+  const tracePath = (path, idx) =>
+    idx === 0
+      ? [path[idx]]
+      : [path[idx], ...expandPath(path[idx], path[idx - 1]), ...tracePath(path, idx - 1)];
+
+  /**
    * Returns an array containing all rocks in the given path.
    */
-  const getAllRocksInPath = (() => {
-    const trace = (path, idx) =>
-      idx === 0
-        ? [path[idx]]
-        : [path[idx], ...expandPath(path[idx], path[idx - 1]), ...trace(path, idx - 1)];
-
-    return (path) => trace(path, path.length - 1);
-  })();
+  const getAllRocksInPath = (path) => tracePath(path, path.length - 1);
 
   return (lines) =>
     lines.reduce((acc, line) => [...acc, ...getAllRocksInPath(parseLine(line))], []);
@@ -79,9 +80,15 @@ const print = (sandSource, rockLookup, sandLookup, bounds) => {
   }
 };
 
-const isBlocked = (position, rockLookup, sandLookup) =>
-  rockLookup.has(position) || sandLookup.has(position);
+/**
+ * Is the position blocked by a rock or sand?
+ */
+const isBlocked = (positionString, rockLookup, sandLookup) =>
+  rockLookup.has(positionString) || sandLookup.has(positionString);
 
+/**
+ * Returns the extremes of the given positions.
+ */
 const findBounds = (positions) => {
   const bounds = (values) => [Math.min(...values), Math.max(...values)];
   const [left, right] = bounds(positions.map(({ x }) => x));
@@ -89,6 +96,9 @@ const findBounds = (positions) => {
   return { left, right, bottom, top };
 };
 
+/**
+ * Is the given position out of the bounds?
+ */
 const outOfBounds = (position, bounds) => {
   if (position.y < bounds.bottom || position.y > bounds.top) {
     return true;
@@ -99,17 +109,28 @@ const outOfBounds = (position, bounds) => {
   return false;
 };
 
+/**
+ * Move the sand until it comes to rest. If the sand cannot move the original position is returned.
+ */
 const moveSand = (position, rockLookup, sandLookup) => {
+  // move down first.
   let desired = add(position, down);
+  // if blocked move down left.
   if (isBlocked(desired.toString(), rockLookup, sandLookup)) {
     desired = add(position, downLeft);
   }
+  // if block move down right.
   if (isBlocked(desired.toString(), rockLookup, sandLookup)) {
     desired = add(position, downRight);
   }
+  // if blocked, sand is at rest, return original position.
   return isBlocked(desired.toString(), rockLookup, sandLookup) ? position : desired;
 };
 
+/**
+ * Move a particle of sand from the source until it either comes to rest
+ * or falls out of bounds.
+ */
 const produceSand = (source, rockLookup, sandLookup, bounds) => {
   let position = source;
   // move until comes to rest or out of bounds.
@@ -131,18 +152,18 @@ export const levelOne = ({ lines }) => {
   const sandSource = new Vector2(500, 0);
   const rocks = parseLines(lines);
   const rockLookup = new Set(rocks.map((x) => x.toString()));
-  const sandAtRest = new Set();
+  const sandLookup = new Set();
   const bounds = findBounds([sandSource, ...rocks]);
 
   while (true) {
-    const newSandPosition = produceSand(sandSource, rockLookup, sandAtRest, bounds);
+    const newSandPosition = produceSand(sandSource, rockLookup, sandLookup, bounds);
     if (!newSandPosition) {
       break;
     }
-    sandAtRest.add(newSandPosition.toString());
+    sandLookup.add(newSandPosition.toString());
   }
 
-  return sandAtRest.size;
+  return sandLookup.size;
 };
 
 /**
