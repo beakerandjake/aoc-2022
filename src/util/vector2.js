ad62dc30f18d59dfeb85883f48a687f2bc47b942
beakerandjake <beakerandjake@users.noreply.github.com>
2023-04-17 10:03:42 -0400

    add some vector util

--- src/util/vector2.js
+++ src/util/vector2.js
@@ -13,6 +13,13 @@ export class Vector2 {
 }
 
 /**
+ * Returns true if the two vectors equal.
+ * @param {Vector2} lhs
+ * @param {Vector2} rhs
+ */
+export const equals = (lhs, rhs) => lhs.x === rhs.x && lhs.y === rhs.y;
+
+/**
  * Combine the two vectors.
  * @param {Vector2} lhs
  * @param {Vector2} rhs
@@ -25,3 +32,12 @@ export const add = (lhs, rhs) => new Vector2(lhs.x + rhs.x, lhs.y + rhs.y);
  * @param {Vector2} rhs
  */
 export const distanceSquared = (lhs, rhs) => (lhs.y - rhs.y) ** 2 + (lhs.x - rhs.x) ** 2;
+
+export const up = new Vector2(0, -1);
+export const down = new Vector2(0, 1);
+export const left = new Vector2(-1, 0);
+export const right = new Vector2(1, 0);
+export const downLeft = add(down, left);
+export const downRight = add(down, right);
+export const upLeft = add(up, left);
+export const upRight = add(up, right);
