651cb50d2b33edd7527210e5be5a7b950c863aa1
beakerandjake <beakerandjake@users.noreply.github.com>
2023-08-19 12:05:10 -0700

    cache input for day 20

--- src/day_20.js
+++ src/day_20.js
@@ -2,6 +2,21 @@
  * Contains solutions for Day 20
  * Puzzle Description: https://adventofcode.com/2022/day/20
  */
+import { toNumber } from './util/string.js';
+import { arrayToString } from './util/array.js';
+
+// rollover from start, swap forward
+// rollover from end, swap backward
+// backward with no rollover, swap backward
+// forward with no rollover, swap forward
+
+// use secondary array, index in lines up to original array index, value is current index
+
+// fn to calculate new index with rollover
+
+// find index of zero
+
+const parseInput = (lines) => lines.map(toNumber);
 
 /**
  * Returns the solution for level one of this puzzle.
@@ -10,8 +25,10 @@
  * @param {String[]} args.lines - Array containing each line of the input string.
  * @returns {Number|String}
  */
-export const levelOne = ({ input, lines }) => {
-  // your code here
+export const levelOne = ({ lines }) => {
+  const encrypted = parseInput(lines);
+  console.log(arrayToString(encrypted));
+  return 10;
 };
 
 /**
