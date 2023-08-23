67f390816e7acbf366363c3730378de6063e6b68
beakerandjake <beakerandjake@users.noreply.github.com>
2023-08-23 16:38:50 -0700

    attempt to scan outside of droplet

--- src/day_18.js
+++ src/day_18.js
@@ -3,7 +3,7 @@
  * Puzzle Description: https://adventofcode.com/2022/day/18
  */
 import { Vector3, up, down, left, right, forward, back, add } from './util/vector3.js';
-import { sum } from './util/array.js';
+import { bounds, sum } from './util/array.js';
 import { toNumber } from './util/string.js';
 
 /**
@@ -31,25 +31,132 @@ const cubeLookup = (cubes) =>
 const sides = [up, down, left, right, forward, back];
 
 /**
- * Return the count of the cubes sides which are not covered by another cube.
- */
-const countExposedSides = (cube, lookup) =>
-  sides
-    .map((side) => add(cube, side).toString())
-    .reduce((acc, side) => (lookup.has(side) ? acc : acc + 1), 0);
-
-/**
  * Returns the solution for level one of this puzzle.
  */
-export const levelOne = ({ lines }) => {
-  const cubes = parseInput(lines);
-  const lookup = cubeLookup(cubes);
-  return sum(cubes.map((cube) => countExposedSides(cube, lookup)));
-};
+export const levelOne = (() => {
+  /**
+   * Return the count of the cubes sides which are not covered by another cube.
+   */
+  const countExposedSides = (cube, lookup) =>
+    sides
+      .map((side) => add(cube, side).toString())
+      .reduce((acc, side) => (lookup.has(side) ? acc : acc + 1), 0);
+
+  return ({ lines }) => {
+    const cubes = parseInput(lines);
+    const lookup = cubeLookup(cubes);
+    return sum(cubes.map((cube) => countExposedSides(cube, lookup)));
+  };
+})();
 
 /**
  * Returns the solution for level two of this puzzle.
  */
-export const levelTwo = ({ input, lines }) => {
-  // your code here
-};
+export const levelTwo = (() => {
+  const rayCast = (origin, direction, distance, lavaDroplet) => {
+    let traveled = 0;
+    let current = origin;
+    while (traveled++ <= distance) {
+      if (lavaDroplet.has(current.toString())) {
+        return true;
+      }
+      current = add(current, direction);
+    }
+    return false;
+  };
+
+  const exposedLeft = (xBounds, yBounds, zBounds, lavaDroplet) => {
+    let exposedCount = 0;
+    const distance = zBounds[1] - zBounds[0];
+    for (let z = zBounds[0]; z <= zBounds[1]; z++) {
+      for (let y = yBounds[0]; y <= yBounds[1]; y++) {
+        if (rayCast(new Vector3(xBounds[0], y, z), right, distance, lavaDroplet)) {
+          exposedCount += 1;
+        }
+      }
+    }
+    return exposedCount;
+  };
+
+  const exposedRight = (xBounds, yBounds, zBounds, lavaDroplet) => {
+    let exposedCount = 0;
+    const distance = zBounds[1] - zBounds[0];
+    for (let z = zBounds[0]; z <= zBounds[1]; z++) {
+      for (let y = yBounds[0]; y <= yBounds[1]; y++) {
+        if (rayCast(new Vector3(xBounds[1], y, z), left, distance, lavaDroplet)) {
+          exposedCount += 1;
+        }
+      }
+    }
+    return exposedCount;
+  };
+
+  const exposedTop = (xBounds, yBounds, zBounds, lavaDroplet) => {
+    let exposedCount = 0;
+    const distance = yBounds[1] - yBounds[0];
+    for (let z = zBounds[0]; z <= zBounds[1]; z++) {
+      for (let x = xBounds[0]; x <= xBounds[1]; x++) {
+        if (rayCast(new Vector3(x, yBounds[1], z), down, distance, lavaDroplet)) {
+          exposedCount += 1;
+        }
+      }
+    }
+    return exposedCount;
+  };
+
+  const exposedBottom = (xBounds, yBounds, zBounds, lavaDroplet) => {
+    let exposedCount = 0;
+    const distance = yBounds[1] - yBounds[0];
+    for (let z = zBounds[0]; z <= zBounds[1]; z++) {
+      for (let x = xBounds[0]; x <= xBounds[1]; x++) {
+        if (rayCast(new Vector3(x, yBounds[0], z), up, distance, lavaDroplet)) {
+          exposedCount += 1;
+        }
+      }
+    }
+    return exposedCount;
+  };
+
+  const exposedFront = (xBounds, yBounds, zBounds, lavaDroplet) => {
+    let exposedCount = 0;
+    const distance = zBounds[1] - zBounds[0];
+    for (let y = yBounds[0]; y <= yBounds[1]; y++) {
+      for (let x = xBounds[0]; x <= xBounds[1]; x++) {
+        if (rayCast(new Vector3(x, y, zBounds[0]), back, distance, lavaDroplet)) {
+          exposedCount += 1;
+        }
+      }
+    }
+    return exposedCount;
+  };
+
+  const exposedBack = (xBounds, yBounds, zBounds, lavaDroplet) => {
+    let exposedCount = 0;
+    const distance = zBounds[1] - zBounds[0];
+    for (let y = yBounds[0]; y <= yBounds[1]; y++) {
+      for (let x = xBounds[0]; x <= xBounds[1]; x++) {
+        if (rayCast(new Vector3(x, y, zBounds[1]), forward, distance, lavaDroplet)) {
+          exposedCount += 1;
+        }
+      }
+    }
+    return exposedCount;
+  };
+
+  return ({ lines }) => {
+    const cubes = parseInput(lines);
+    const lookup = cubeLookup(cubes);
+    const xBounds = bounds(cubes.map(({ x }) => x));
+    const yBounds = bounds(cubes.map(({ y }) => y));
+    const zBounds = bounds(cubes.map(({ z }) => z));
+    const counts = [
+      exposedLeft(xBounds, yBounds, zBounds, lookup),
+      exposedRight(xBounds, yBounds, zBounds, lookup),
+      exposedTop(xBounds, yBounds, zBounds, lookup),
+      exposedBottom(xBounds, yBounds, zBounds, lookup),
+      exposedFront(xBounds, yBounds, zBounds, lookup),
+      exposedBack(xBounds, yBounds, zBounds, lookup),
+    ];
+    return sum(counts);
+  };
+})();
