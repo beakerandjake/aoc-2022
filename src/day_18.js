1269dcb2e80a9f637d6218dfc3be58091b6f6049
beakerandjake <beakerandjake@users.noreply.github.com>
2023-08-23 15:04:05 -0700

    cleanup

--- src/day_18.js
+++ src/day_18.js
@@ -6,18 +6,33 @@ import { Vector3, up, down, left, right, forward, back, add } from './util/vecto
 import { arrayToString } from './util/array.js';
 import { toNumber } from './util/string.js';
 
+/**
+ * Parse a single line of input and return a new Vector3.
+ */
 const parseLine = (line) => new Vector3(...line.split(',').map(toNumber));
 
+/**
+ * Parse each line of input and return the positions of the cubes.
+ */
 const parseInput = (lines) => lines.map(parseLine);
 
+/**
+ * Returns a set containing the position of each cube.
+ */
 const cubeLookup = (cubes) =>
   cubes.reduce((acc, x) => {
     acc.add(x.toString());
     return acc;
   }, new Set());
 
+/**
+ * Vectors which can be added to a cubes position to return the position adjacent to each face of the cube.
+ */
 const sides = [up, down, left, right, forward, back];
 
+/**
+ * Return the count of the cubes sides which are not covered by another cube.
+ */
 const exposedSideCount = (cube, lookup) =>
   sides
     .map((side) => add(cube, side).toString())
