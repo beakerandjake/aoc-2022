fdaa7e17ae10965ea76efb5e9b1f380dceb114fd
beakerandjake <beakerandjake@users.noreply.github.com>
2023-08-23 15:08:52 -0700

    cleanup

--- src/day_18.js
+++ src/day_18.js
@@ -3,7 +3,7 @@
  * Puzzle Description: https://adventofcode.com/2022/day/18
  */
 import { Vector3, up, down, left, right, forward, back, add } from './util/vector3.js';
-import { arrayToString } from './util/array.js';
+import { sum } from './util/array.js';
 import { toNumber } from './util/string.js';
 
 /**
@@ -33,7 +33,7 @@ const sides = [up, down, left, right, forward, back];
 /**
  * Return the count of the cubes sides which are not covered by another cube.
  */
-const exposedSideCount = (cube, lookup) =>
+const countExposedSides = (cube, lookup) =>
   sides
     .map((side) => add(cube, side).toString())
     .reduce((acc, side) => (lookup.has(side) ? acc : acc + 1), 0);
@@ -44,7 +44,7 @@ const exposedSideCount = (cube, lookup) =>
 export const levelOne = ({ lines }) => {
   const cubes = parseInput(lines);
   const lookup = cubeLookup(cubes);
-  return cubes.reduce((acc, cube) => acc + exposedSideCount(cube, lookup), 0);
+  return sum(cubes.map((cube) => countExposedSides(cube, lookup)));
 };
 
 /**
