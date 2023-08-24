abbbfa1cc77b4e5b4a33d32350dac9fcc410c270
beakerandjake <beakerandjake@users.noreply.github.com>
2023-08-24 08:50:08 -0700

    cache input for day 23

--- src/day_23.js
+++ src/day_23.js
@@ -5,13 +5,10 @@
 
 /**
  * Returns the solution for level one of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
  */
 export const levelOne = ({ input, lines }) => {
   // your code here
+  return 1234;
 };
 
 /**
