b74c5a05c5e523b8ebc21723452cfae930a00444
beakerandjake <beakerandjake@users.noreply.github.com>
2023-08-23 20:57:38 -0700

    testing flood fill

--- src/day_18.js
+++ src/day_18.js
@@ -6,39 +6,21 @@ import { Vector3, up, down, left, right, forward, back, add } from './util/vecto
 import { bounds, sum } from './util/array.js';
 import { toNumber } from './util/string.js';
 
-/**
- * Parse a single line of input and return a new Vector3.
- */
 const parseLine = (line) => new Vector3(...line.split(',').map(toNumber));
 
-/**
- * Parse each line of input and return the positions of the cubes.
- */
 const parseInput = (lines) => lines.map(parseLine);
 
-/**
- * Returns a set containing the position of each cube.
- */
 const cubeLookup = (cubes) =>
   cubes.reduce((acc, x) => {
     acc.add(x.toString());
     return acc;
   }, new Set());
 
-/**
- * Vectors which can be added to a cubes position to return the position adjacent to each face of the cube.
- */
 const sides = [up, down, left, right, forward, back];
 
-/**
- * Returns the positions of each side surrounding the cube which are exposed.
- */
 const getExposedSides = (cube, lookup) =>
   sides.map((side) => add(cube, side)).filter((side) => !lookup.has(side.toString()));
 
-/**
- * Returns the solution for level one of this puzzle.
- */
 export const levelOne = ({ lines }) => {
   const cubes = parseInput(lines);
   const lookup = cubeLookup(cubes);
@@ -59,7 +41,7 @@ export const levelTwo = (() => {
         });
       return acc;
     }, {});
-    const q = Object.values(z).filter((x) => x >= 6);
+    const q = Object.values(z).filter((x) => x !== 6);
     return sum(q);
   };
 
@@ -71,9 +53,80 @@ export const levelTwo = (() => {
       .map((side) => add(cube, side).toString())
       .reduce((acc, side) => (lookup.has(side) ? acc : acc + 1), 0);
 
+  const findBounds = (cubes) => {
+    const [xMin, xMax] = bounds(cubes.map(({ x }) => x));
+    const [yMin, yMax] = bounds(cubes.map(({ y }) => y));
+    const [zMin, zMax] = bounds(cubes.map(({ z }) => z));
+    return {
+      left: xMin,
+      right: xMax,
+      bottom: yMin,
+      top: yMax,
+      back: zMin,
+      front: zMax,
+    };
+  };
+
+  const pointIsOutsideWorld = (point, worldBounds) => {
+    if (point.x < worldBounds.left || point.x > worldBounds.right) {
+      return true;
+    }
+    if (point.y < worldBounds.bottom || point.y > worldBounds.top) {
+      return true;
+    }
+    if (point.z < worldBounds.back || point.z > worldBounds.front) {
+      return true;
+    }
+    return false;
+  };
+
+  const pointsOutsideCube = (worldBounds, lookup) => {
+    const queue = [new Vector3(worldBounds.left, worldBounds.bottom, worldBounds.back)];
+    const examined = new Set();
+    const toReturn = [];
+    while (queue.length) {
+      const point = queue.shift();
+      const pointKey = point.toString();
+
+      if (examined.has(pointKey)) {
+        continue;
+      }
+
+      if (!pointIsOutsideWorld(point, worldBounds) && !lookup.has(point.toString())) {
+        examined.add(pointKey);
+        toReturn.push(point);
+        queue.push(add(point, left));
+        queue.push(add(point, right));
+        queue.push(add(point, down));
+        queue.push(add(point, up));
+        queue.push(add(point, forward));
+        queue.push(add(point, back));
+      }
+    }
+    return toReturn;
+  };
+
   return ({ lines }) => {
     const cubes = parseInput(lines);
     const lookup = cubeLookup(cubes);
-    return getAllExposedSides(cubes, lookup);
+    const worldBounds = findBounds(cubes);
+    console.log('world bounds', worldBounds);
+    const outsideCube = pointsOutsideCube(worldBounds, lookup);
+    console.log('outside cube', outsideCube.length);
+    console.log('cube', cubes.length);
+    // for (let x = xMin; x <= xMax; x++) {
+    //   for (let y = yMin; y <= yMax; y++) {
+    //     for (let z = zMin; z <= zMax; z++) {
+    //       console.
+    //     }
+    //   }
+    // }
+
+    // have a world cube of x*y*z, every point on the cube is either outside space, a point of lava or an air pocket inside of lava.
+    // start from outer boundaries and flood fill entire outside of lava..
+    // if a point is not flood filled and is not part of lava, then it must be an air pocket.
+    // once know air pocket locations, can ignore exposed edges which touch an air pocket.
+
+    return 1234;
   };
 })();
