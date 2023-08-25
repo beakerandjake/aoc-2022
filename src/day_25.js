aec0b857f6908d578acf39b560323e49c2247b41
beakerandjake <beakerandjake@users.noreply.github.com>
2023-08-25 14:12:38 -0700

    first pass day 25

--- src/day_25.js
+++ src/day_25.js
@@ -36,12 +36,10 @@ const decimalToSnafu = (decimal) => {
   let borrow = 0;
   const output = [];
   for (let index = 0; index < quantities.length; index++) {
-    let quantity = quantities[index] || 0;
-    if (borrow && quantity < 3) {
-      quantity += 1;
-    }
-    output.push(decimalToSnafuChars[quantity]);
-    borrow = quantity > 2 ? 1 : 0;
+    const originalQuantity = quantities[index] || 0;
+    const modifiedQuantity = borrow ? (originalQuantity + 1) % 5 : originalQuantity;
+    output.push(decimalToSnafuChars[modifiedQuantity]);
+    borrow = originalQuantity > 2 || modifiedQuantity > 2 ? 1 : 0;
   }
   if (borrow) {
     output.push('1');
@@ -51,51 +49,10 @@ const decimalToSnafu = (decimal) => {
 
 /**
  * Returns the solution for level one of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
  */
-export const levelOne = ({ input, lines }) => {
-  // console.log(4890);
-  // console.log(snafuToDecimal)'1-11-2'));
-  const decimals = [1747, 906, 198, 11, 201, 31, 1257, 32, 353, 107, 7, 3, 37];
-  const snafus = [
-    '1=-0-2',
-    '12111',
-    '2=0=',
-    '21',
-    '2=01',
-    '111',
-    '20012',
-    '112',
-    '1=-1=',
-    '1-12',
-    '12',
-    '1=',
-    '122',
-  ];
-
-  // console.log('decimal to snafu', arraysEqual(decimals, snafus.map(snafuToDecimal)));
-  // console.log(
-  //   'snafu to decimal',
-  //   arraysEqual(decimals.map(decimalToSnafu), snafuToDecimal)
-  // );
-
-  // console.log(arrayToString(decimals.map(decimalToSnafu)));
-  
-  console.log(decimalToSnafu(1747));
-  //
-  // const decimals = lines.map(snafuToDecimal);
-  // return decimalToSnafu(sum(decimals));
-  return 1234;
-};
+export const levelOne = ({ lines }) => decimalToSnafu(sum(lines.map(snafuToDecimal)));
 /**
  * Returns the solution for level two of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
  */
 export const levelTwo = ({ input, lines }) => {
   // your code here
