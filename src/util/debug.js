544c1fe6128ffabf54612f9ee82cde421d2bcaa6
beakerandjake <beakerandjake@users.noreply.github.com>
2023-08-25 18:23:41 -0700

    add util fn

--- src/util/debug.js
+++ src/util/debug.js
@@ -1,5 +1,6 @@
 import { EOL } from 'node:os';
 import { findBounds, toLookup, Vector2 } from './vector2.js';
+import { elementAt2d } from './array2d.js';
 import { range } from './array.js';
 
 /**
@@ -24,4 +25,21 @@ export const worldToString = (points, charset = { empty: '.', occupied: '#' }) =
         .join('')
     )
     .join(EOL);
+};
+
+/**
+ * Returns a string visualizing the flattened 2d array.
+ * @param {Array} array - A flat 2d array.
+ */
+export const array2dToString = (array, shape) => {
+  const { width, height } = shape;
+  const rows = [];
+  for (let y = 0; y < height; y++) {
+    const row = [];
+    for (let x = 0; x < width; x++) {
+      row.push(elementAt2d(array, shape, y, x));
+    }
+    rows.push(row);
+  }
+  return rows.map((row) => row.join('')).join(EOL);
 };
