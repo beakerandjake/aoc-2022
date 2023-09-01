9e73fe7ad502f64d9e4473595ce4f18f0304f132
beakerandjake <beakerandjake@users.noreply.github.com>
2023-08-31 20:26:50 -0700

    use dynamic programming style

--- src/day_16.js
+++ src/day_16.js
@@ -25,10 +25,10 @@ const parseLines = (lines) =>
     .reduce((acc, { name, ...nodeData }) => ({ ...acc, [name]: nodeData }), {});
 
 /**
- * Performs a BFS from root node to all other nodes in the graph.
- * Returns an object which maps each node to the size of the shortest path for that node.
+ * Returns an object which maps each node to the length of the shortest path from that node to all other nodes.
  */
-const bfs = (graph, rootKey) => {
+const nodeDistances = (graph, rootKey) => {
+  // Use BFS to find the shortest path to other nodes.
   const queue = [rootKey];
   const history = {};
   while (queue.length) {
@@ -64,7 +64,7 @@ const getChoices = (graph, travelCosts, visitedNodes, minutes, pressure) =>
 
 const maximumRelease = (graph, startNodeKey, minutes) => {
   const travelCosts = Object.keys(graph).reduce(
-    (acc, key) => ({ ...acc, [key]: bfs(graph, key) }),
+    (acc, key) => ({ ...acc, [key]: nodeDistances(graph, key) }),
     {}
   );
   const queue = [...getChoices(graph, travelCosts[startNodeKey], new Set(), minutes, 0)];
@@ -83,16 +83,67 @@ const maximumRelease = (graph, startNodeKey, minutes) => {
 };
 
 /**
+ * Returns the maximum pressure that can be released in the given time starting from the start node.
+ */
+const findMaximumPressure = (graph, startNodeKey, totalTime) => {
+  // create a lookup which maps a node key to the distances to each node.
+  const travelCosts = Object.keys(graph).reduce(
+    (acc, key) => ({ ...acc, [key]: nodeDistances(graph, key) }),
+    {}
+  );
+
+  // return a hash code of the state.
+  const hashCode = (time, opened) => `${time}_${[...opened].sort().join('_')}`;
+
+  // object which will map a state to its maximum value.
+  const memo = {};
+
+  const topDown = (key, time, pressure, opened) => {
+    const stateHash = hashCode(time, opened);
+
+    // if this state has already been calculated return the memoized value.
+    if (memo[stateHash]) {
+      return memo[stateHash];
+    }
+
+    if (time <= 0) {
+      return pressure;
+    }
+
+    const results = [];
+
+    // travel to each unopened neighbor and get the result if we opened that value.
+    for (const [neighborKey, travelTime] of Object.entries(travelCosts[key])) {
+      const { flowRate } = graph[neighborKey];
+      const remainingTime = time - travelTime - 1;
+      if (!opened.has(neighborKey) && flowRate > 0 && remainingTime > 0) {
+        results.push(
+          topDown(
+            neighborKey,
+            remainingTime,
+            flowRate * remainingTime + pressure,
+            new Set([...opened, neighborKey])
+          )
+        );
+      }
+    }
+
+    const result = results.length ? Math.max(...results) : pressure;
+    memo[stateHash] = result;
+    return result;
+  };
+
+  return topDown(startNodeKey, totalTime, 0, new Set());
+};
+
+/**
  * Returns the solution for level one of this puzzle.
  * @param {Object} args - Provides both raw and split input.
  * @param {String} args.input - The original, unparsed input string.
  * @param {String[]} args.lines - Array containing each line of the input string.
  * @returns {Number|String}
  */
-export const levelOne = ({ lines }) => {
-  const graph = parseLines(lines);
-  return maximumRelease(graph, 'AA', 30);
-};
+export const levelOne = ({ lines }) => findMaximumPressure(parseLines(lines), 'AA', 30);
 
 /**
  * Returns the solution for level two of this puzzle.
