a26f0a68397454879d240923fa639f8bef129aef
beakerandjake <beakerandjake@users.noreply.github.com>
2023-08-31 22:18:52 -0700

    finish clean up level 1

--- src/day_16.js
+++ src/day_16.js
@@ -43,45 +43,6 @@ const nodeDistances = (graph, rootKey) => {
   return history;
 };
 
-const getChoices = (graph, travelCosts, visitedNodes, minutes, pressure) =>
-  Object.entries(travelCosts)
-    // skip already opened nodes.
-    .filter(([key]) => !visitedNodes.has(key))
-    // skip nodes which have no flow rate. F
-    .filter(([key]) => graph[key].flowRate > 0)
-    // calculate the new state if move to and open node.
-    .map(([key, travelCost]) => {
-      // include one minute it takes to open the valve.
-      const remainingTime = minutes - travelCost - 1;
-      return {
-        current: key,
-        remainingTime,
-        pressure: graph[key].flowRate * remainingTime + pressure,
-        visited: new Set([...visitedNodes, key]),
-      };
-    })
-    .filter(({ remainingTime }) => remainingTime > 0);
-
-const maximumRelease = (graph, startNodeKey, minutes) => {
-  const travelCosts = Object.keys(graph).reduce(
-    (acc, key) => ({ ...acc, [key]: nodeDistances(graph, key) }),
-    {}
-  );
-  const queue = [...getChoices(graph, travelCosts[startNodeKey], new Set(), minutes, 0)];
-  let maximum = 0;
-
-  while (queue.length) {
-    const { current, visited, pressure, remainingTime } = queue.shift();
-    if (pressure > maximum) {
-      maximum = pressure;
-    }
-    queue.push(
-      ...getChoices(graph, travelCosts[current], visited, remainingTime, pressure)
-    );
-  }
-  return maximum;
-};
-
 /**
  * Returns the maximum pressure that can be released in the given time starting from the start node.
  */
@@ -99,48 +60,33 @@ const findMaximumPressure = (graph, startNodeKey, totalTime) => {
   const memo = {};
 
   // recursively find the max value in a top down manner
-  const topDown = (key, time, pressure, opened) => {
-    // no time left means the pressure we have is the pressure we got.
-    if (time <= 0) {
-      return pressure;
-    }
-
+  const topDown = (costs, time, pressure, opened) => {
     // return value if already memoized.
     const stateHash = hashCode(time, opened);
     if (memo[stateHash]) {
       return memo[stateHash];
     }
 
-    // const results = Object.keys(travelCosts[key]).reduce((acc, targetKey) => {
-    //   const { flowRate } = graph[targetKey];
-    //   const newTime = time - travelCosts[key][targetKey] - 1;
-    //   if (!opened.has(targetKey) && flowRate > 0 && newTime > 0) {
-    //     const newPressure = flowRate * newTime + pressure;
-    //     const newVisited = new Set([...opened, targetKey]);
-    //     acc.push(topDown(targetKey, newTime, newPressure, newVisited));
-    //   }
-    //   return acc;
-    // }, []);
-
-    const results = [];
-    // travel to each unopened neighbor and get the result if we opened that value.
-    for (const targetKey of Object.keys(travelCosts[key])) {
+    // recursively find the max value by visiting unopened nodes.
+    const maxPressure = Object.keys(costs).reduce((max, targetKey) => {
       const { flowRate } = graph[targetKey];
-      const newTime = time - travelCosts[key][targetKey] - 1;
+      const newTime = time - costs[targetKey] - 1;
       if (!opened.has(targetKey) && flowRate > 0 && newTime > 0) {
         const newPressure = flowRate * newTime + pressure;
         const newVisited = new Set([...opened, targetKey]);
-        results.push(topDown(targetKey, newTime, newPressure, newVisited));
+        const result = topDown(travelCosts[targetKey], newTime, newPressure, newVisited);
+        return result > max ? result : max;
       }
-    }
+      return max;
+    }, pressure);
 
-    const result = Math.max(...results, pressure);
-    // memoize this result so we don't have to recalculate it again.
-    memo[stateHash] = result;
-    return result;
+    // memoize the pressure released by this state so we don't have to recalculate it.
+    memo[stateHash] = maxPressure;
+
+    return maxPressure;
   };
 
-  return topDown(startNodeKey, totalTime, 0, new Set());
+  return topDown(travelCosts[startNodeKey], totalTime, 0, new Set());
 };
 
 /**
