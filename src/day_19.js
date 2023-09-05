20bdbc874a18bc49d9bf8f245cb2454bf2c71c5a
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-05 15:12:04 -0700

    wip attempting to solve recursively

--- src/day_19.js
+++ src/day_19.js
@@ -6,34 +6,31 @@ import { arraysEqual, sum, updateAt, arrayToString } from './util/array.js';
 import { writeToFile } from './util/io.js';
 import { toNumber } from './util/string.js';
 
-/**
- * Regex to capture all numbers in a string.
- */
-const digitRegex = /(\d+)/g;
-
 const blueprintToString = ({ id, costs }) =>
   `id: ${id}, costs: [${costs.map((cost) => arrayToString(cost)).join(', ')}]`;
 
 /**
- * Parse a blueprint from the input.
- */
-const parseLine = (line) => {
-  const matches = line.match(digitRegex).map(toNumber);
-  return {
-    id: matches[0],
-    costs: [
-      [matches[1], 0, 0, 0], // Ore
-      [matches[2], 0, 0, 0], // Clay
-      [matches[3], matches[4], 0, 0], // Obsidian
-      [matches[5], 0, matches[6], 0], // Geode
-    ],
-  };
-};
-
-/**
  * Parse a blueprint from each line of the input.
  */
-const parseBlueprints = (lines) => lines.map(parseLine);
+const parseBlueprints = (lines) => {
+  /**
+   * Parse a blueprint from the input.
+   */
+  const parseLine = (line) => {
+    const matches = line.match(/(\d+)/g).map(toNumber);
+    return {
+      id: matches[0],
+      costs: [
+        [matches[1], 0, 0, 0], // Ore
+        [matches[2], 0, 0, 0], // Clay
+        [matches[3], matches[4], 0, 0], // Obsidian
+        [matches[5], 0, matches[6], 0], // Geode
+      ],
+    };
+  };
+
+  return lines.map(parseLine);
+};
 
 /**
  * Returns the element in the ore position.
@@ -73,11 +70,14 @@ const subtract = (lhs, rhs) => lhs.map((x, index) => x - rhs[index]);
  */
 const gather = (robots) => [...robots];
 
+const buildRobot = (robots, index) =>
+  robots.map((count, i) => (i === index ? count + 1 : index));
+
 /**
  * Are there enough resources to build the robot?
  */
 const canAffordRobot = (resources, buildCost) =>
-  buildCost.every((resource, index) => resource <= resources[index]);
+  buildCost.every((cost, index) => cost <= resources[index]);
 
 /**
  * Returns a new array representing the robots after adding a new robot.
@@ -85,15 +85,56 @@ const canAffordRobot = (resources, buildCost) =>
 const buildNewRobot = (robots, typeIndex) =>
   updateAt(robots, typeIndex, robots[typeIndex] + 1);
 
-const solve = (time, robots, resources) => {
-  if (time <= 0) {
-    return resources;
+/**
+ * Compares the two resource arrays.
+ * Returns > 0 if b is greater than a.
+ * Returns < 0 if b is less than a.
+ * Returns 0 if b is equal to a.
+ */
+const compareResources = (a, b) => {
+  for (let i = 4; i--; ) {
+    if (b[i] === a[i]) {
+      continue;
+    }
+    return b[i] - a[i];
   }
-
-  const resourcesThisTurn = add(resources, gather(robots));
-
-  return solve(time - 1, robots, resourcesThisTurn);
+  return 0;
 };
+
+const solve = (totalTime, startingRobots, startingResources, costs) => {
+  const memo = {};
+
+  const recurse = (time, robots, resources) => {
+    // const hash = `${time}.${robots.join('.')}.${resources.join('.')}`;
+    // const hash = `${time}.${robots.join('.')}`;
+    // if (memo[hash]) {
+    //   return memo[hash];
+    // }
+    if (time <= 0) {
+      return { value: resources, robots };
+    }
+    const gatheredThisTurn = gather(robots);
+    const results = [recurse(time - 1, robots, add(resources, gatheredThisTurn))];
+    if (time > 1) {
+      for (let i = 0; i < costs.length; i++) {
+        const cost = costs[i];
+        if (canAffordRobot(resources, cost)) {
+          const newRobots = buildRobot(robots, i);
+          const newResources = add(gatheredThisTurn, subtract(resources, cost));
+          // results.push(recurse(time - 1, newRobots, newResources));
+        }
+      }
+    }
+
+    const best = results.sort((a, b) => compareResources(a.value, b.value))[0];
+    // memo[hash] = best;
+    return best;
+  };
+
+  return recurse(totalTime, startingRobots, startingResources);
+};
+
+const scale = (array, value) => array.map((x) => x * value);
 
 /**
  * Returns the solution for level one of this puzzle.
@@ -106,7 +147,7 @@ export const levelOne = ({ lines }) => {
   const blueprints = parseBlueprints(lines);
   const robots = [1, 0, 0, 0];
   const resources = [0, 0, 0, 0];
-  const result = solve(24, robots, resources);
+  const result = solve(24, robots, resources, blueprints[0].costs);
   console.log(result);
   return 1234;
 };
