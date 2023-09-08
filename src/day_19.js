3bb03aac0c3f9eb99467637f205d964a2b258826
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-07 19:46:07 -0700

    clean up

--- src/day_19.js
+++ src/day_19.js
@@ -10,6 +10,16 @@ import { toNumber } from './util/string.js';
 const resourceTypes = 4;
 
 /**
+ * Maps resource type to its index in a robots/resource array.
+ */
+const indexes = {
+  ore: 0,
+  clay: 1,
+  obsidian: 2,
+  geodes: 3,
+};
+
+/**
  * Parse the puzzle input and return an array of blueprints.
  */
 const parseBlueprints = (lines) => {
@@ -41,9 +51,28 @@ const parseBlueprints = (lines) => {
 };
 
 /**
+ * Returns the number of obsidian.
+ */
+const clay = (array) => array[indexes.clay];
+
+/**
+ * Returns the number of obsidian.
+ */
+const obsidian = (array) => array[indexes.obsidian];
+
+/**
  * Returns the number of geodes.
  */
-const geodes = (items) => items[3];
+const geodes = (array) => array[indexes.geodes];
+
+/**
+ * Returns the sum of the items in the resources / robots array.
+ */
+const sum = (array) =>
+  array[indexes.ore] +
+  array[indexes.clay] +
+  array[indexes.obsidian] +
+  array[indexes.geodes];
 
 /**
  * Returns a new array by adding each element of lhs to each corresponding element of rhs.
@@ -119,7 +148,7 @@ const pruneIfCantProduceEnoughGeodes = (current, best) => {
   // every minute for the remaining time.
   let geodeRobots = geodes(current.robots);
   let estimated = geodes(current.resources);
-  for (let i = 0; i < current.time; i++) {
+  for (let i = current.time; i--; ) {
     estimated += geodeRobots;
     geodeRobots++;
   }
@@ -151,25 +180,19 @@ const pruneIfIdledTooLong = (totalTime) => {
   return ({ time, robots }) => {
     // prune if not built any robot by the cutoff time.
     if (time <= buildSomethingCutoff && time > clayCutoff) {
-      return robots[0] + robots[1] + robots[2] + robots[3] === 1;
+      return sum(robots) === 1;
     }
     // prune if not built a clay robot by the cutoff time.
     if (time <= clayCutoff && time > obsidianCutoff) {
-      return robots[1] === 0;
+      return clay(robots) === 0;
     }
     // prune if not built an obsidian robot by the cutoff time.
     if (time <= obsidianCutoff) {
-      return robots[2] === 0;
+      return obsidian(robots) === 0;
     }
     return false;
   };
 };
-
-/**
- * Returns true if have enough resources to cover the build cost.
- */
-const canAfford = (resources, buildCost) =>
-  resources.every((resource, type) => resource >= buildCost[type]);
 
 /**
  * Returns the new state which result from building nothing this turn.
@@ -181,43 +204,50 @@ const doNothing = ({ time, robots, resources }) => ({
 });
 
 /**
+ * Returns true if have enough resources to cover the build cost.
+ */
+const canAfford = ({ resources }, buildCost) =>
+  resources.every((resource, type) => resource >= buildCost[type]);
+
+/**
+ * Returns true if currently have enough robots of this type to cover maximum required per turn.
+ */
+const robotIsMaxedOut = ({ robots }, { maxCosts }, type) =>
+  robots[type] >= maxCosts[type];
+
+/**
+ * Returns true if has more of this resource stockpiled than can spend in the remaining time.
+ */
+const resourceIsMaxedOut = ({ resources, time }, { maxCosts }, type) =>
+  resources[type] >= maxCosts[type] * time;
+
+/**
  * Returns the new state which results from building the specified robot this turn.
  */
-const buildRobot = (time, robots, resources, buildCost, type) => ({
+const buildRobot = ({ time, robots, resources }, buildCost, type) => ({
   time: time - 1,
   robots: increment(robots, type),
   resources: add(robots, subtract(resources, buildCost)),
 });
 
 /**
- * Returns true if currently have enough robots of this type to cover maximum required per turn.
- */
-const robotIsMaxedOut = (robots, maxCosts, type) => robots[type] >= maxCosts[type];
-
-/**
- * Returns true if has more of this resource stockpiled than can spend in the remaining time.
- */
-const resourceIsMaxedOut = (resources, time, maxCosts, type) =>
-  resources[type] >= maxCosts[type] * time;
-
-/**
  * Returns an array containing robots that can be built this turn and the resulting next state respectively.
  */
-const buildRobots = ({ time, robots, resources }, { costs, maxCosts }) => {
+const buildRobots = (state, blueprint) => {
   // if can afford a geode robot then dont worry about other robots just build geode.
-  if (canAfford(resources, geodes(costs))) {
-    return [buildRobot(time, robots, resources, geodes(costs), 3)];
+  if (canAfford(state, geodes(blueprint.costs))) {
+    return [buildRobot(state, geodes(blueprint.costs), indexes.geodes)];
   }
 
   const toReturn = [];
-  for (let type = resourceTypes - 1; type--; ) {
-    const buildCost = costs[type];
+  for (let type = indexes.geodes; type--; ) {
+    const buildCost = blueprint.costs[type];
     if (
-      canAfford(resources, buildCost) &&
-      !robotIsMaxedOut(robots, maxCosts, type) &&
-      !resourceIsMaxedOut(resources, time, maxCosts, type)
+      canAfford(state, buildCost) &&
+      !robotIsMaxedOut(state, blueprint, type) &&
+      !resourceIsMaxedOut(state, blueprint, type)
     ) {
-      toReturn.push(buildRobot(time, robots, resources, buildCost, type));
+      toReturn.push(buildRobot(state, buildCost, type));
     }
   }
   return toReturn;
