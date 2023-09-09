7db3ae90b341a649227dc9eae1b728af8291c233
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-08 17:14:05 -0700

    feeling out bit fields

--- src/day_24.js
+++ src/day_24.js
@@ -3,25 +3,63 @@
  * Puzzle Description: https://adventofcode.com/2022/day/24
  */
 
-import { convertTo2dArray } from './util/array2d.js';
+import { convertTo2dArray, index2d, map2d } from './util/array2d.js';
+import { binaryToString } from './util/bitwise.js';
+import { array2dToString } from './util/debug.js';
+import { invert } from './util/object.js';
 
-const bitIndex = {
-  blizzardNorth: 0,
-  blizzardEast: 1,
-  blizzardWest: 2,
-  blizzardSouth: 3,
-  boundary: 
+const bits = {
+  empty: 0,
+  blizzardUp: 1,
+  blizzardDown: 1 << 1,
+  blizzardRight: 1 << 2,
+  blizzardLeft: 1 << 3,
+  boundary: 1 << 4,
+};
+
+const charToBit = {
+  '.': bits.empty,
+  '^': bits.blizzardUp,
+  v: bits.blizzardDown,
+  '>': bits.blizzardRight,
+  '<': bits.blizzardLeft,
+  '#': bits.boundary,
+};
+
+const render = (array) => {
+  const bitToChar = {
+    ...invert(charToBit),
+    0xf: 4,
+    0xe: 3,
+    0xd: 3,
+    0xc: 2,
+    0xb: 3,
+    0xa: 2,
+    0x9: 2,
+    0x7: 3,
+    0x6: 2,
+    0x5: 2,
+    0x3: 2,
+  };
+  const toString = array2dToString(array, (char) => bitToChar[char] || '?');
+  console.log(toString);
 };
 
 const parseMap = (lines) => {
-  const height = lines.length;
-  const width = lines[0].length;
-  return convertTo2dArray(lines, (char, y, x) => {
-    if (char === '#') {
-      return '&';
+  convertTo2dArray(lines, (char) => charToBit[char]);
+}
+
+const moveBlizzards = (map) => {
+  const copy = [...map.items];
+  const yMax = map.shape.height - 1;
+  const xMax = map.shape.height - 1;
+  for (let y = 1; y < yMax; y++) {
+    for (let x = 1; x < xMax; x++) {
+      const index = index2d(map.shape.height, y, x);
+      copy[index] = '@';
     }
-    return char;
-  });
+  }
+  return { ...map, items: copy };
 };
 
 /**
@@ -29,7 +67,9 @@ const parseMap = (lines) => {
  */
 export const levelOne = ({ lines }) => {
   const map = parseMap(lines);
-  console.log(map);
+  render(map);
+  const moved = moveBlizzards(map);
+  render(moved);
   return 1234;
 };
 
